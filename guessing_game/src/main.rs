3b88238a30eed3b0b27967b5d958ad601421042b
westofer <westoferRaymond@gmail.com>
2020-09-27 10:58:16 +0300

    Guessing game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,34 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    // println!("The secret_number is {}", secret_number);
+
+    loop {
+        println!("Please Input your number");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("you guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
+    }
+}
